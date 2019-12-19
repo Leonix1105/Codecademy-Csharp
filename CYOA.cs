5c4266b2006da94e6e3820f46515aee6edebaa31
Brian <49199654+Leonix1105@users.noreply.github.com>
2019-12-19 13:21:20 -0600

    Update CYOA.cs

--- CYOA.cs
+++ CYOA.cs
@@ -7,4 +7,27 @@ namespace ChooseYourOwnAdventure
     static void Main(string[] args)
     {
       /* THE MYSTERIOUS NOISE */
+      // Start by asking for the user's name
+      Console.Write("What is your name? ");
+      string name = Console.ReadLine();
+      Console.WriteLine($"Hello, {name}! Welcome to our story");
+      Console.WriteLine("It begins on a cold rainy night. You're sitting in your room and hear a noise coming from down the hall. Do you investigate?");
       
+      Console.Write("Type YES or NO\n");
+      string noiseChoice = Console.Readline();
+      noiseChoice = noiseChoice.ToUpper();
+      
+      if (noiseChoice == "NO") {
+        Console.WriteLine("Not much of an adventure if we don't leave our room!");
+      } else if (noiseChoice == "YES") {
+        Console.WriteLine("You walk into the hallway and see a light coming from under a door down the hall.");
+        Console.WriteLine("You walk towards it. Do you open it or knock?");
+      }
+      
+      Console.WriteLine("Type OPEN or KNOCK\n");
+      string doorChoice = Console.Readline();
+      doorChoice = doorChoice.ToUpper();
+      
+      if (doorChoice == "KNOCK") {
+        
+      }
