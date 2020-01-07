0605c828a5a9391bc9b700793852e753fa5e9b2a
Brian <49199654+Leonix1105@users.noreply.github.com>
2020-01-07 11:07:46 -0600

    2
    Added the rest of the story

--- CYOA.cs
+++ CYOA.cs
@@ -29,5 +29,34 @@ namespace ChooseYourOwnAdventure
       doorChoice = doorChoice.ToUpper();
       
       if (doorChoice == "KNOCK") {
+        Console.WriteLine("A voice behind the door speaks. It says, \"Answer this riddle: \"");
+        Console.WriteLine("\"Poor people have it. Rich people need it. If you eat it you die. What is it \"");
+        Console.WriteLine("Type your answer: \n");
+        string riddleAnswer = Console.ReadLine();
+        riddleAnswer = riddleAnswer.ToUpper();
+        if (riddleAnswer == "NOTHING")
+        {
+          Console.WriteLine("Correct. The door opens to nothing");
+        }
+        
+      } else if (doorChoice == "OPEN") {
+           
+          Console.WriteLine("The door is locked! See if one of your three keys will open it.");
+          Console.WriteLine("Enter a number (1-3:)\n");
+          string keyChoice = Console.ReadLine();
+        
+          switch (keyChoice) {
+            case "1":
+              Console.WriteLine("You choose the first key. Lucky Choice! The door opens and NOTHING is there. Strange...");
+              break;
+            case "2":
+              Console.WriteLine("You choose the second key. The door doesn't open.");
+              break;
+            case "3":
+              Console.WriteLine("Poor choice. As you turn the key, a trap door opens and you fall to your death.");
+              break;
+          }
+      }  
+      Console.WriteLine("THE END"); 
         
       }
