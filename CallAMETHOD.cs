5f56c1c328bf53d5bba0168470d1ab63c628187e
Brian <49199654+Leonix1105@users.noreply.github.com>
2020-01-07 15:08:15 -0600

    Create CallAMETHOD.cs

---
+++ CallAMETHOD.cs
@@ -0,0 +1,15 @@
+using System;
+namespace CallAMethod
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string msg = "Yabba Dabba Doo!";
+      int msg1 = Math.Min(3,8);
+      Console.WriteLine(msg);
+      string a = msg.Substring(0,1);
+      Console.WriteLine(a);
+    }
+  }
+}
